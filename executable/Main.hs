b4b39357567d7739d2c298e4f612f2d682e5797f
Huseyin Yilmaz <huseyinyilmaz@huseyinyilmazMBP-8.local>
2016-12-17 00:03:35 -0800

    Updated repository with haskeleton template

---
+++ executable/Main.hs
@@ -0,0 +1,5 @@
+import Foundation
+import qualified Exchange
+
+main :: IO ()
+main = Exchange.main
---
+++ library/Exchange.hs
@@ -0,0 +1,8 @@
+-- | An example module.
+module Exchange (main) where
+
+import Foundation
+
+-- | An example function.
+main :: IO ()
+main = return ()
