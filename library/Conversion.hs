657e6d784c03fd4d6e9bd960f4425992c39f1829
Huseyin Yilmaz <huseyinyilmaz@huseyinyilmazMBP-8.local>
2016-12-20 17:22:33 -0800

    Work on pipeline and cli interface.

---
+++ library/Conversion.hs
@@ -0,0 +1,42 @@
+module Conversion where
+
+import qualified Data.Vector as V
+import Pipes
+import qualified Pipes.Prelude as P
+import qualified System.IO as IO
+import qualified Pipes.ByteString as PB
+import qualified Pipes.Text.IO as PTIO
+import qualified Data.Text as Text
+import qualified Data.Text.Lazy.Encoding as Encoding
+import qualified Data.Text.Lazy as LazyText
+import qualified Pipes.Csv as PCsv
+import Control.Monad (forever)
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Aeson as Aeson
+
+addNewLine :: Text.Text -> Text.Text
+addNewLine t = Text.concat [t, "\n"]
+
+toJSON :: V.Vector Text.Text ->  Proxy () (V.Vector Text.Text) () Text.Text IO ()
+toJSON headers = forever $ do
+  vals <- await
+  let res = Aeson.Object $ HM.fromList (V.toList $ V.zip headers (V.map Aeson.String vals))
+  (yield . addNewLine . LazyText.toStrict . Encoding.decodeUtf8 . Aeson.encode) res
+
+copyFiles :: FilePath -> FilePath -> IO ()
+copyFiles inFile outFile =
+  IO.withFile inFile IO.ReadMode
+  (\hin -> IO.withFile outFile IO.WriteMode (handleFile hin))
+  where
+    handleFile hin hout = do
+      -- producer :: Producer (Either String (V.Vector Text.Text)) IO ()
+      let eitherProducer :: Producer (Either String (V.Vector Text.Text)) IO ()
+          eitherProducer = (PCsv.decode PCsv.NoHeader (PB.fromHandle hin))
+          producer :: Producer (V.Vector Text.Text) IO ()
+          producer = eitherProducer >-> P.concat
+          consumer = PTIO.toHandle hout
+      e <- next producer
+      case e of
+        Left () -> putStrLn "No lines!"
+        Right (headers, bodyProducer) -> do
+          runEffect ( bodyProducer >-> (toJSON headers) >-> consumer)
--- library/Exchange.hs
+++ library/Exchange.hs
@@ -1,59 +1,21 @@
 -- | An example module.
 module Exchange (main) where
-
--- import qualified Data.ByteString.Lazy as BL
--- import qualified Data.ByteString as B
-
--- import qualified Data.Csv as Csv
-import qualified Data.Vector as V
-import Pipes
--- import qualified Pipes.Safe.Prelude as PSP
-import qualified Pipes.Prelude as P
-import qualified System.IO as IO
--- import qualified Pipes.Safe as PS
-import qualified Pipes.ByteString as PB
+import Conversion
+import Turtle
 import qualified Data.Text as Text
-import qualified Pipes.Csv as PCsv
--- import Pipes.Text.IO (fromHandle)
-import Control.Monad (forever)
---import Control.Pipe.Common
--- import qualified Data.Text as Text
+
+description :: Description
+description = "Convert a csv file into a json file."
+
+fileArgs :: Parser (Text, Text)
+fileArgs = ((,) <$> (optText "input" 'i'  "input csv file.")
+                <*> (optText "output" 'o'  "output json file."))
+
 -- | An example function.
--- main :: IO ()
--- main = do
---   -- csvData <- BL.readFile "/Users/huseyinyilmaz/radius/RADIUS_US_EXTRACT_10_2016.txt"
---   csvData <- BL.readFile "/Users/huseyinyilmaz/test.csv"
---   case decode NoHeader csvData of
---     Left err -> putStrLn err
---     Right v -> putStrLn $ show $ (v::V.Vector (V.Vector BL.ByteString))
-
--- unpack :: Proxy () Text.Text () String IO ()
--- unpack = forever $ do
---     x <- await
---     yield $ Text.unpack x
-
-
-showPipe :: Proxy () (Either String (V.Vector Text.Text)) () String IO b
-showPipe = forever $ do
-    x::(Either String (V.Vector Text.Text)) <- await
-    yield $ show x
-
-
 main :: IO ()
 main = do
-  IO.withFile "/Users/huseyinyilmaz/test.csv"
-              IO.ReadMode
-              (\handle -> do
-                  let producer = (PCsv.decode PCsv.NoHeader (PB.fromHandle handle))
-                  headers <- P.head producer
-                  putStrLn "Header"
-                  putStrLn $ show headers
-                  putStrLn $ "Rows"
-                  runEffect ( producer>->
-                              (showPipe) >->
-                              P.stdoutLn)
-               )
-  -- csvData <- BL.readFile "/Users/huseyinyilmaz/test.csv"
-  -- case Csv.decode Csv.NoHeader csvData of
-  --   Left err -> putStrLn err
-  --   Right v -> putStrLn $ show $ (v::V.Vector (V.Vector Text.Text))
+  (inFile, outFile) <- options description fileArgs
+  copyFiles (fromString $ Text.unpack inFile) (fromString $ Text.unpack outFile)
+  -- where
+  --   inFile = "/Users/huseyinyilmaz/radius/RADIUS_US_EXTRACT_10_2016.txt"
+  --   outFile = "/Users/huseyinyilmaz/val.json"
